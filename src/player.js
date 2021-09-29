78ea9f2f76197b688fe90ccc9af982131c9785eb
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-09-29 09:36:46 -0400

    Add renderLi function to Player class

--- src/player.js
+++ src/player.js
@@ -10,8 +10,18 @@ class Player {
         this.team_id = team_id; // Need to create elements based on this object
         this.element = document.createElement('li');
         this.element.dataset['id'] = id;        //specific identifyer for each li element based on their id
-        this .element.id = `player-${id}`
+        this.element.id = `player-${id}`
         Player.all.push(this)
-        debugger
+    }
+
+    renderLi(){         //No need to pass an argument through
+        this.element.innerHTML = `
+        <div data-id="${this.id}">
+        <h3 class="name">${this.name}</h3>
+        <h4 class="position">${this.position}</h4>
+        <p class="description">${this.description}</p>
+        </div>
+        `
+        return this.element
     }
 }
