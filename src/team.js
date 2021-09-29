aa815464592de546f035b6770b9670ec3424134a
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-09-29 13:11:16 -0400

    Update Team Constructor

--- src/team.js
+++ src/team.js
@@ -1,8 +1,14 @@
 class Team {
+
+    static all = [];
+
     constructor(id, name, players){
         this.id = id;
         this.name = name;
         this.players = players;
-
+        this.element = document.createElement('li');
+        this.element.dataset['id'] = id;        
+        this.element.id = `team-${id}`
+        Team.all.push(this)
     }
 }
