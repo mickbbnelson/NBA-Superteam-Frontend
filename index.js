064178adf26192fa44ba8e9026fe331dc4f1bcf2
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-09-29 11:17:59 -0400

    Add getTeams fetch function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const urlPort = `http://localhost:3000`;
 const playerApiCall = new PlayerApi(urlPort);
+const teamApiCall = new TeamApi(urlPort);
 
 //playerApiCall.getPlayers()
+
+teamApiCall.getTeams()
--- src/teamAPI.js
+++ src/teamAPI.js
@@ -2,4 +2,10 @@ class TeamApi {
     constructor(urlPort){
         this.urlPort = urlPort 
     }
+
+    getTeams(){
+        fetch(this.urlPort + `/teams`)
+        .then(r => r.json())
+        .then(data => console.log(data))
+    }
 }
