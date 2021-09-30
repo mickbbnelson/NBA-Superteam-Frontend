0841c585b4a393cb7a7a9145d17a34045f7fa493
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-09-30 14:47:01 -0400

    Add form variable and form event listener

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const urlPort = `http://localhost:3000`;
 const playerApiCall = new PlayerApi(urlPort);
 const teamApiCall = new TeamApi(urlPort);
+const form = document.getElementById("team-form")
 
-playerApiCall.getPlayers()
+//playerApiCall.getPlayers()
 
 teamApiCall.getTeams()
+
+form.addEventListener('submit', handleSubmit)
